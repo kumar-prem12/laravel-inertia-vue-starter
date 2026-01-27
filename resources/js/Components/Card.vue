a1e796690120767bab7a1a5b73997aba060c4401
prem <prem72965@gmail.com>
2026-01-28 00:23:52 +0530

    Add listing

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+    listing: Object,
+})
+</script>
+
+<template>
+    <div class="bg-white rounded-lg shadow-lg overflow-hidden dark:bg-slate-700 h-full flex flex-col justify-between">
+        <Link href="">
+            <img 
+            :src="listing.image ? `/storage/${listing.image}` : '/storage/images/listing/tiger.jpg' "
+             alt="image" class="w-full h-48 object-cover object-center bg-slate-300"/>
+        </Link>
+        <div class="p-4">
+            <h3 class="font-bold text-xl mb-2">
+                {{ listing.title.substring(0, 30) }}...
+            </h3>
+            <p> Listed on  {{ new Date(listing.created_at).toLocaleDateString() }} by 
+                <button class="text-link">
+                    {{ listing.user.name}}
+                </button>
+            </p>
+        </div>
+        <div v-if="listing.tags" class="flex items-center gap-3 px-4 pb-4">
+            <div v-for="tag in listing.tags.split(',')" :key="tag">
+                <button class="bg-slate-500 text-white px-2 py-px rounded-full hover:bg-slate-700 dark:hover:bg-slate-900">
+                    {{ tag }}
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,11 +1,23 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
+import Card from '../Components/Card.vue';
 
+defineProps({
+    listings: Object,
+});
 </script>
 
 <template>
-    <Head title="- Home"></Head>
-    <header>
+    {{console.log(listings)}}
+    <Head title="- Listings"></Head>
+    <!-- <header>
         <p>This is a simple starter kit for Laravel 11, Inertia JS, and Vue JS, without authentication. Tailwind and ZiggyVue are installed.</p>
-    </header>
+    </header> -->
+    <div v-if="Object.keys(listings.data).length">
+        <div class="grid grid-cols-3 gap-4">
+            <div v-for="listing in listings.data" :key="listing.id">
+                <Card :listing="listing" />
+            </div>
+        </div>
+    </div>
 </template>
